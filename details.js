ec78437f64bb0befe3545820273b301d579e1294
rafistern1986 <rsternbuch@gmail.com>
2014-12-05 10:24:55 +0200

    Update details.js

--- details.js
+++ details.js
@@ -1,1 +1,38 @@
-s
+function getSpesificParmeterThatCameInUrl(theName) {
+    var longSearch = window.location.search;
+    longSearch = longSearch.replace("?", "");
+    var searchAsArray = longSearch.split("&");
+    for (var i = 0; i < searchAsArray.length; i++) {
+        var cutArray = searchAsArray[i].split("=");
+        if (cutArray[0] === theName) {
+            return cutArray[1].split("%20").join(" ");
+        }
+    }
+}
+$(document).ready(function () {
+    var whatBookDidClientClick = getSpesificParmeterThatCameInUrl("whithBook");
+    var arrayFromLocalStorage = JSON.parse(localStorage.getItem("arrayInStorage_asSring"));
+    for (var i = 0; i < arrayFromLocalStorage.length; i++) {
+        if (arrayFromLocalStorage[i].id == whatBookDidClientClick) {
+            addNewObjectToList(arrayFromLocalStorage[i])
+        }
+    }
+})
+
+function addNewObjectToList(obj) {
+    var name = obj.name.toUpperCase()
+    var linkToGoogel = $("<a>").attr("href", "https://www.google.co.il/?#q=" + obj.name + "+season+" + obj.season + "+episode+" + obj.episode).text("googel it").attr("target", "_blank");
+    var linkToTrailer = $("<a>").attr("href", "https://www.youtube.com/results?search_query=" + obj.name + "+season+" + obj.season + "+episode+" + obj.episode + "+trailer").text("watch trailer").attr("target", "_blank");
+    var name = $("<h1>").text(name);
+    var season = $("<div>").text(obj.season);
+    var episode = $("<div>").text(obj.episode);
+    var score = $("<div>").text(obj.score);
+    var dateAdded = $("<div>");
+    var status = $("<div>");
+    var details = $("<div>").append(linkToGoogel);
+    var trailer = $("<div>").append(linkToTrailer);
+    var theWholeLi = $("<li>").append(season, episode, score, dateAdded, status, details, trailer);
+    $("div.theNameInBig").append(name);
+    $("ul.theListOfShows").append(theWholeLi);
+
+};
